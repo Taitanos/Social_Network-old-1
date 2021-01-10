92e604f272a6d1f1d0a5910a0fff46c370193d5a
Taitanos <hayaashi87@gmail.com>
2021-01-10 20:42:54 +0300

    Breakdown into components (Header, NavBar,Profile)

--- src/App.js
+++ src/App.js
@@ -1,53 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Profile from "./components/Profile";
+
 
 const App = () => {
-  return (
-      <div className='app-wrapper'>
-        <header className='header'>
-          <img src="https://s1.logaster.com/static/v3/img/products/logo.png"/>
-        </header>
-        <nav className='nav'>
-            <div>
-                <a>Profile</a>
-            </div>
-            <div>
-                <a>Messages</a>
-            </div>
-            <div>
-                <a>News</a>
-            </div>
-            <div>
-                <a>Music</a>
-            </div>
-            <div>
-                <a>Setting</a>
-            </div>
-        </nav>
-        <div className='content'>
-           <div>
-               <img src='https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/advice/maps-satellite-images/satellite-image-of-globe.jpg' />
-            </div>
-            <div>
-                ava
-            </div>
-            <div>
-                My posts
-                <div>
-                    New post
-                </div>
-                <div>
-                    <div>
-                        Post 1
-                    </div>
-                    <div>
-                        Post 2
-                    </div>
-                </div>
-            </div>
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <NavBar/>
+            <Profile/>
         </div>
-      </div>
-  )
+    )
 }
 
 export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img
+                src='https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/advice/maps-satellite-images/satellite-image-of-globe.jpg'/>
+        </div>
+        <div>
+            ava
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+    </div>
+
+}
+
+export default Profile;
